3d22e05cfb31c5c7cb07ca0529c55bb02fe3aec6
chaseoes <chase.oes@gmail.com>
2013-03-21 15:39:00 -0700

    Initial commit.

---
+++ src/me/chaseoes/novillagertrading/NoVillagerTrading.java
@@ -0,0 +1,33 @@
+package me.chaseoes.novillagertrading;
+
+import net.minecraft.server.v1_4_R1.MerchantRecipeList;
+import net.minecraft.server.v1_4_R1.NBTTagCompound;
+
+import org.bukkit.craftbukkit.v1_4_R1.entity.CraftVillager;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoVillagerTrading extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onEntityEnteract(PlayerInteractEntityEvent event) {
+        if (!event.getPlayer().hasPermission("novillagertrading.bypass")) {
+            Entity e = event.getRightClicked();
+            if (e instanceof Villager) {
+                NBTTagCompound nbt = new NBTTagCompound();
+                nbt.setCompound("Offers", new MerchantRecipeList().a());
+                ((CraftVillager) (Villager) e).getHandle().a(nbt);
+            }
+        }
+    }
+
+}
